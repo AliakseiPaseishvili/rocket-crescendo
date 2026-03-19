a5daa21ccbc524f303e1fbea9f52cd632d5da7cb
Aliaksei Paseishvili <aliaksei.paseishvili@gmail.com>
2026-03-19 19:39:53 +0400

    Added shadcn ui

--- frontend/features/landing/components/Landing.tsx
+++ frontend/features/landing/components/Landing.tsx
@@ -1,9 +1,13 @@
 'use client';
 
+import { Button } from '@/frontend/components/ui/button';
 import { useTranslation } from 'react-i18next';
 
 export const Landing = () => {
   const { t } = useTranslation('common');
 
-  return <div>{t('hello')}</div>;
+  return <div>
+    {t('hello')}
+    <Button>Save</Button>
+  </div>;
 };
