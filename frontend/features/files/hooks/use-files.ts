123ec84528f76a49a18273a304e9b54b1f22ec88
Aliaksei Paseishvili <aliaksei.paseishvili@gmail.com>
2026-04-13 10:04:47 +0400

    Added debounce hook

--- frontend/features/files/hooks/use-files.ts
+++ frontend/features/files/hooks/use-files.ts
@@ -4,12 +4,15 @@ import { useQuery } from '@tanstack/react-query';
 
 import type { FileFilter } from '@/backend/features/file';
 import { api } from '@/frontend/features/api';
+import { useDebounce } from '@/frontend/features/hooks';
 
 import { FILES_QUERY_KEY } from '../constants';
 
 export function useFiles(filter?: FileFilter) {
+  const debouncedFilter = useDebounce(filter);
+
   return useQuery({
-    queryKey: filter ? [FILES_QUERY_KEY, filter] : [FILES_QUERY_KEY],
-    queryFn: () => api.getFiles(filter ? { query: filter } : undefined),
+    queryKey: [FILES_QUERY_KEY, debouncedFilter],
+    queryFn: () => api.getFiles(debouncedFilter ? { query: debouncedFilter } : undefined),
   });
 }
